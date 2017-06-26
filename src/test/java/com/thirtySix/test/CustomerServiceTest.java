9b316daad3499f54f5464f8784637d85a54b5649
AbnerLin <shaoyang0805@gmail.com>
2017-06-26 09:18:50 +0800

    comment the unuse code.

--- src/test/java/com/thirtySix/test/CustomerServiceTest.java
+++ src/test/java/com/thirtySix/test/CustomerServiceTest.java
@@ -1,22 +1,22 @@
-package com.thirtySix.test;
-
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.thirtySix.core.Buffer;
-import com.thirtySix.service.CustomerService;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = TestConfig.class)
-public class CustomerServiceTest {
-
-	@Autowired
-	private CustomerService customerService = null;
-	
-	@Autowired
-	private Buffer buffer = null;
-	
-	
-}
+//package com.thirtySix.test;
+//
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//import com.thirtySix.core.Buffer;
+//import com.thirtySix.service.CustomerService;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = TestConfig.class)
+//public class CustomerServiceTest {
+//
+//	@Autowired
+//	private CustomerService customerService = null;
+//	
+//	@Autowired
+//	private Buffer buffer = null;
+//	
+//	
+//}
